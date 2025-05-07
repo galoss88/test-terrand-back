4628be094b336f101f101e36dd4e2f4349855fad
Nicolas ivan gallo garcia <gallogarcianicolas@gmail.com>
2025-05-07 05:11:27 -0300

    feat: implement authentication middleware for user verification and access control

---
+++ src/middlewares/auth.middleware.ts
@@ -0,0 +1,62 @@
+import { NextFunction, Request, Response } from "express";
+import jwt from "jsonwebtoken";
+
+declare global {
+  namespace Express {
+    interface Request {
+      user?: {
+        id: string;
+        email: string;
+      };
+    }
+  }
+}
+
+const authMiddleware = (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const authHeader = req.headers.authorization;
+
+    if (!authHeader || !authHeader.startsWith("Bearer ")) {
+      res.status(401).json({
+        success: false,
+        message: "Acceso no autorizado. Token no proporcionado.",
+      });
+      return;
+    }
+
+    const token = authHeader.split(" ")[1];
+
+    const secret = process.env.JWT_SECRET;
+    if (!secret) {
+      throw new Error("JWT_SECRET no configurado");
+    }
+
+    const decoded = jwt.verify(token, secret) as {
+      sub: string;
+      email: string;
+    };
+
+    //Asignamos los datos de usuarios en la request para ser usado posteriormente
+    req.user = {
+      id: decoded.sub,
+      email: decoded.email,
+    };
+
+    next();
+  } catch (error) {
+    if (error instanceof jwt.JsonWebTokenError) {
+      res.status(401).json({
+        success: false,
+        message: "Token inválido o expirado",
+      });
+    }
+
+    console.error("Error en middlware de autenticación:", error);
+    res.status(500).json({
+      success: false,
+      message: "Error interno del servidor",
+    });
+  }
+};
+
+export default authMiddleware;
--- src/routes/index.route.ts
+++ src/routes/index.route.ts
@@ -1,9 +1,10 @@
 import { Router } from "express";
+import authMiddleware from "../middlewares/auth.middleware";
 import privateRoutes from "./private/index.route";
 import publicRoutes from "./public/index.route";
 const router = Router();
 
 router.use("/public", publicRoutes);
-router.use("/private", privateRoutes);
+router.use("/private", authMiddleware, privateRoutes);
 
 export default router;

2c1abe07a3e1d3a7f2f26978965b80fe2858e10b
Nicolas ivan gallo garcia <gallogarcianicolas@gmail.com>
2025-05-07 13:21:41 -0300

    as

--- dist/db/typeOrm/config.js
+++ dist/db/typeOrm/config.js
@@ -10,22 +10,9 @@ const typeorm_1 = require("typeorm");
 const entities_1 = require("./entities");
 // Cargar variables de entorno
 dotenv_1.default.config();
-// —— DEBUG: comprobar variables —— 
-console.log("╔═🛠️  ENV VARIABLES 🛠️═╗");
-console.log("DB_HOST:", process.env.DB_HOST);
-console.log("DB_PORT:", process.env.DB_PORT);
-console.log("DB_USERNAME:", process.env.DB_USERNAME);
-console.log("DB_PASSWORD:", process.env.DB_PASSWORD ? "****" : undefined);
-console.log("DB_NAME:", process.env.DB_NAME);
-console.log("DB_SYNC:", process.env.DB_SYNC);
-console.log("DB_LOGGING:", process.env.DB_LOGGING);
-console.log("NODE_ENV:", process.env.NODE_ENV);
-console.log("PORT:", process.env.PORT);
-console.log("database name", process.env.DATABASE_URL);
-console.log("═══════════════════════");
 exports.AppDataSource = new typeorm_1.DataSource({
     type: "postgres",
-    url: process.env.DATABASE_URL || "postgresql://postgres:qLAMRAuuwEaYZvWbGxusLnxekcPyhQTR@postgres.railway.internal:5432/railway",
+    url: process.env.DATABASE_URL,
     // host: process.env.DB_HOST || "localhost",
     // port: parseInt(process.env.DB_PORT || "5432"),
     // username: process.env.DB_USERNAME || "postgres",

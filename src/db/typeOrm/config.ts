ecb1a2a50b559ffd7a58ec3e92728c7519de9000
Nicolas ivan gallo garcia <gallogarcianicolas@gmail.com>
2025-05-07 12:41:56 -0300

    fix: set default database type to 'postgres' in DataSource configuration

--- src/db/typeOrm/config.ts
+++ src/db/typeOrm/config.ts
@@ -7,7 +7,7 @@ import { RecipeEntity, UserEntity } from "./entities";
 dotenv.config();
 
 export const AppDataSource = new DataSource({
-  type: process.env.DB_TYPE ?? ("postgres" as "postgres"),
+  type:  "postgres",
   host: process.env.DB_HOST || "localhost",
   port: parseInt(process.env.DB_PORT || "5432"),
   username: process.env.DB_USERNAME || "postgres",
